cfced68d9e4190c430b35d7a003361fad2d5947a
Roushan Kumar <roushankumarcool@gmail.com>
2023-06-12 04:46:31 +0530

    BE

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
-
-const connect=mongoose.connect("mongodb://127.0.0.1:27017/google_notes")
+require("dotenv").config()
+const connect=mongoose.connect(process.env.mongoURL)
 
 module.exports={
     connect
